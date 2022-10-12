32f650a84c55e3486e59f2eb71c69fba7fb95fb1
Mariu <oi_mario@live.com>
2022-10-11 23:38:48 -0300

    subindo tudo de uma vez
    6 hrs do zero

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
+from urllib import response
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
 # arrumar bug en compatibilidade
 from spacy.cli import download
-
 download("en_core_web_sm")
 
 
-# class ENGSM:
-
-#   ISO_639_1 = 'en_core_web_sm'
+class ENGSM:
+    ISO_639_1 = 'en_core_web_sm'
 
 # CODIGO COMEÇA AQUI tagger_language=ENGSM
 
@@ -24,8 +23,25 @@ talk = [
     "Nada não, só lol mesmo",
     "E a mina?",
     "Foi de nanos na mae",
-    "Foda..."
+    "Complicado..."
 ]
 
+# resposta do chat:
 trainer = ListTrainer(chatbot)
 trainer.train(talk)
+
+
+while True:
+    talk = input("Mande uma mensagem para o chatbot:")
+    if talk == "parar":
+        break
+    resposta = chatbot.get_response(talk)
+    print(resposta)
+
+# falar com o bot manual:
+
+chatbot.get_response("e a mina?")
+
+# limpar armazenamento
+
+chatbot.storage.drop()
